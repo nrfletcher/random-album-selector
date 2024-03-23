76aafa5ac9047f9055358f5d4dd8ab60b6070243
riley <89424122+nrfletcher@users.noreply.github.com>
2024-03-23 18:35:06 -0400

    ignore

--- script.py
+++ script.py
@@ -1,4 +1,14 @@
-import os
+import urllib.request
 
-s = os.getpid()
-print(s)
+url = 'https://en.wikipedia.org/wiki/Wikipedia:WikiProject_Albums/500'
+file_name = 'wikipedia_page.html'
+
+# Fetch the HTML content
+with urllib.request.urlopen(url) as response:
+    html_content = response.read()
+
+# Save the HTML content to a file
+with open(file_name, 'wb') as file:
+    file.write(html_content)
+
+print(f"HTML content has been saved to {file_name}")
